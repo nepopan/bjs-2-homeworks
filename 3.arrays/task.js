45bda43c1065ee4465f2b0682a1f16797cce148e
Pavel Tarazevich <pashatarazevich@gmail.com>
2023-07-01 18:37:43 +0300

    Исправлено согласно замечаниям преподавателя

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,5 @@
 function compareArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  return arr1.every((value, index) => value === arr2[index]);
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
