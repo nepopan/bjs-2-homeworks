a3459313394287a1bd9e86a4245c33e4d5edb208
Pavel Tarazevich <pashatarazevich@gmail.com>
2023-07-01 16:02:48 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+  const filteredUsers = users.filter(user => user.gender === gender);
   
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  const sumOfAges = filteredUsers
+    .map(user => user.age)
+    .reduce((sum, age) => sum + age, 0);
+
+  return sumOfAges / filteredUsers.length;
 }
